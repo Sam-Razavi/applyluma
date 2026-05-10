40177f871d333c6b5598f0c531d30afdb786126a
Sam Sahbaei Razavi <sam.sahbaei.razavi@iths.se>
2026-05-10 21:40:38 +0200

    fix(phase8): use scraper extracted skills in dbt mart

--- dbt/models/intermediate/int_jobs_with_skills.sql
+++ dbt/models/intermediate/int_jobs_with_skills.sql
@@ -9,7 +9,16 @@ with deduped as (
 skills_extracted as (
     select
         *,
-        {{ extract_skills('job_description') }} as skill_array
+        case
+            when jsonb_typeof(extracted_skills) = 'array'
+                 and jsonb_array_length(extracted_skills) > 0
+                then (
+                    select array_agg(lower(trim(skill_text)))
+                    from jsonb_array_elements_text(extracted_skills) as extracted(skill_text)
+                    where nullif(trim(skill_text), '') is not null
+                )
+            else {{ extract_skills('job_description') }}
+        end as skill_array
     from deduped
 )
 
